ce013d9d86016d73bd57145aae6ad166c617db6a
Your Name <lauraelgozi@gmail.com>
2018-12-20 18:44:05 +0200

    with error and browse

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import os
-from bottle import (get, post, redirect, request, route, run, static_file, template, TEMPLATE_PATH, error)
+from bottle import (get, post, redirect, request, route, run, static_file, template, TEMPLATE_PATH, jinja2_view, error)
 import json
-from functools import partial
-
-TEMPLATE_PATH.insert(0, '')
 import utils
+from functools import partial
+TEMPLATE_PATH.insert(0, '')
+view = partial(jinja2_view, template_lookup=['templates'])
 
 # Static Routes
 
-
-@error(404)
-def error404(error):
-    return template("404.tpl")
 
 
 @get("/js/<filepath:re:.*\.js>")
@@ -35,6 +31,13 @@ def index():
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
 
 
+@error(404)
+def error404(error):
+    sectionTemplate = "./templates/404.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+
 @route('/browse')
 def index():
     sectionTemplate = "./templates/browse.tpl"
@@ -47,6 +50,49 @@ def index():
 def index():
     sectionTemplate = "./templates/search.tpl"
     return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+
+
+@route('/ajax/show/<number>')
+@view('show.tpl')
+def show(number):
+    return template("./templates/show.tpl", version=utils.getVersion(),
+                    result=json.loads(utils.getJsonFromFile(number)))
+
+
+@route('/show/<number>')
+@view("show.tpl")
+def show(number):
+    sectionTemplate = "./templates/show.tpl"
+    return template("./pages/index.html", version=utils.getVersion(),
+                    sectionTemplate=sectionTemplate,
+                    sectionData=json.loads(utils.getJsonFromFile(number)))
+
+
+@route('/show/<number>/episode/<id>')
+@view("episode.tpl")
+def episode(number, id):
+    relevant_data = {}
+    show = json.loads(utils.getJsonFromFile(number))
+    episodes = show["_embedded"]["episodes"]
+    for episode in episodes:
+        if episode["id"] == int(id):
+            relevant_data = episode
+    sectionTemplate = "./templates/episode.tpl"
+    return template("./pages/index.html", version=utils.getVersion(),
+                    sectionTemplate=sectionTemplate,
+                    sectionData=relevant_data)
+
+
+@route('/ajax/show/<number>/episode/<id>')
+@view("episode.tpl")
+def episode(number, id):
+    relevant_data = {}
+    show = json.loads(utils.getJsonFromFile(number))
+    episodes = show["_embedded"]["episodes"]
+    for episode in episodes:
+        if episode["id"] == int(id):
+            relevant_data = episode
+    return template("./templates/episode.tpl", result=relevant_data)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from bottle import template
 import json
 
 JSON_FOLDER = './data'
-AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871","2993", "305"]
+AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871", "2993", "305"]
 
 
 def getVersion():
@@ -14,6 +14,9 @@ def getJsonFromFile(showName):
         return template("{folder}/{filename}.json".format(folder=JSON_FOLDER, filename=showName))
     except:
         return "{}"
+
+
+
 
 
 
